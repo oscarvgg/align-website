51a367fdd2fb915ada9a78ebbaa1f2c38ce3b21d
Oscar Gonzalez <gonzalezgreco@gmail.com>
2023-09-06 20:56:15 +0200

    Add linkedin Tag

--- Sources/AlignWebsite/Theme/Theme+MyTheme.swift
+++ Sources/AlignWebsite/Theme/Theme+MyTheme.swift
@@ -29,7 +29,8 @@ private struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
                 .component(NavigationBar(context: context)),
                 .mainContentWrapper(
                     .component(LandingPage())
-                )
+                ),
+                .linkedInTag()
             )
         )
     }
@@ -53,7 +54,8 @@ private struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
                     )
                 ),
                 .component(WaitingListSection()),
-                .component(FooterSection())
+                .component(FooterSection()),
+                .linkedInTag()
             )
         )
     }
@@ -90,7 +92,8 @@ private struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
                     )
                 ),
                 .component(WaitingListSection()),
-                .component(FooterSection())
+                .component(FooterSection()),
+                .linkedInTag()
             )
         )
     }
@@ -104,7 +107,8 @@ private struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
             .body(
                 .component(NavigationBar(context: context)),
                 .mainContentWrapper(.contentBody(page.body)),
-                .component(FooterSection())
+                .component(FooterSection()),
+                .linkedInTag()
             )
         )
     }
@@ -139,7 +143,8 @@ private struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
                     )
                 ),
                 .component(WaitingListSection()),
-                .component(FooterSection())
+                .component(FooterSection()),
+                .linkedInTag()
             )
         )
     }
@@ -178,7 +183,8 @@ private struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
                     )
                 ),
                 .component(WaitingListSection()),
-                .component(FooterSection())
+                .component(FooterSection()),
+                .linkedInTag()
             )
         )
     }
@@ -306,6 +312,30 @@ private extension Node where Context == HTML.BodyContext {
                     )
                 )
             )
+        )
+    }
+    
+    static func linkedInTag() -> Node {
+        .raw(
+                """
+                <script type="text/javascript">
+                _linkedin_partner_id = "5989689";
+                window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+                </script><script type="text/javascript">
+                (function(l) {
+                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);})(window.lintrk);
+                </script>
+                <noscript>
+                <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=5989689&fmt=gif" />
+                </noscript>
+                """
         )
     }
 }
