1a1e2f7838b4b2dab257f527977bb471a1188b36
Oscar Gonzalez <gonzalezgreco@gmail.com>
2023-09-06 21:37:47 +0200

    add sender modal

--- Sources/AlignWebsite/Theme/Theme+MyTheme.swift
+++ Sources/AlignWebsite/Theme/Theme+MyTheme.swift
@@ -346,6 +346,7 @@ public extension Node where Context == HTML.DocumentContext {
         .group([
             .googleTrackerHead(),
             .senderHead(),
+            .senderModalHead(),
             .facebookPixel()
         ])
     }
@@ -380,6 +381,28 @@ public extension Node where Context == HTML.DocumentContext {
                 m.parentNode.insertBefore(a, m)
               })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
               sender('20f1418bde9678')
+        """
+        ))
+    }
+    
+    static func senderModalHead() -> Node {
+        .head(
+        .script(
+        """
+            <script>
+              (function (s, e, n, d, er) {
+                s['Sender'] = er;
+                s[er] = s[er] || function () {
+                  (s[er].q = s[er].q || []).push(arguments)
+                }, s[er].l = 1 * new Date();
+                var a = e.createElement(n),
+                    m = e.getElementsByTagName(n)[0];
+                a.async = 1;
+                a.src = d;
+                m.parentNode.insertBefore(a, m)
+              })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+              sender('20f1418bde9678')
+            </script>
         """
         ))
     }
