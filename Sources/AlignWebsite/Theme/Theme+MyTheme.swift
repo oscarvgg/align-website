c243b3597cdb9dfe791fffb88cdd7e7124c56077
Oscar Gonzalez <gonzalezgreco@gmail.com>
2023-09-07 20:32:24 +0200

    add microsoft clarity

--- Sources/AlignWebsite/Theme/Theme+MyTheme.swift
+++ Sources/AlignWebsite/Theme/Theme+MyTheme.swift
@@ -345,6 +345,7 @@ public extension Node where Context == HTML.DocumentContext {
     static func addScripts() -> Node {
         .group([
             .googleTrackerHead(),
+            .microsoftClarityHead(),
             .senderHead(),
             .senderModalHead(),
             .facebookPixel()
@@ -362,6 +363,15 @@ public extension Node where Context == HTML.DocumentContext {
     
         gtag('config', 'G-T0WWL4KDTV');
     """
+            ))
+    }
+    
+    static func microsoftClarityHead() -> Node {
+        .head(
+            .script(
+                """
+                <!-- Clarity tracking code for https://align.day/ --><script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "irjgxuuujn");</script>
+                """
             ))
     }
     
