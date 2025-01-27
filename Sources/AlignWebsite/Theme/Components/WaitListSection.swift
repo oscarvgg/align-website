532c8be9388f5f6e0d54a5202c61e6ecbf49ed53
Oscar Gonzalez <gonzalezgreco@gmail.com>
2025-01-27 21:01:29 +0100

    add beta list badge

--- Sources/AlignWebsite/Theme/Components/WaitListSection.swift
+++ Sources/AlignWebsite/Theme/Components/WaitListSection.swift
@@ -27,6 +27,14 @@ Sign up now and be among the first to elevate your time management experience.
 Get ready to align your time and conquer your day.
 """)
                         .class("py-6")
+                      
+                        Node<Any>.raw(
+                           """
+                           <a target="_blank" href="https://betalist.com/startups/align-day?utm_campaign=badge-align-day&amp;utm_medium=badge&amp;utm_source=badge-featured">
+                           <img alt="Align - Time blocking app that helps you plan, organize, and track your daily tasks | BetaList" width="156" height="54" style="width: 156px; height: 54px" src="https://betalist.com/badges/featured?id=117212&amp;theme=color">
+                           </a>
+                           """
+                        )
                     }
                     
                     Div {
