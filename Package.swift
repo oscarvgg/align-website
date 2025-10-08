a215364a9063d9f6585f87f237fad9dc03c0bf69
Oscar Gonzalez <gonzalezgreco@gmail.com>
2025-10-08 17:39:09 +0200

    downgrade publish

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(name: "Publish",
                  url: "https://github.com/johnsundell/publish.git",
-                 from: "0.8.0")
+                 .exact("0.8.0"))
     ],
     targets: [
         .executableTarget(
